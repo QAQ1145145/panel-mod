5a45ea9c09ab9acc3b269fc8708fed12f119a2e2
bfsdo0 <bfsdo0@gmail.com>
2018-11-03 20:17:06 +0800

    每日邮件只显示最新的一个公告

--- app/Command/DailyMail.php
+++ app/Command/DailyMail.php
@@ -17,26 +17,21 @@ class DailyMail
     {
         $users = User::all();
         $logs = Ann::orderBy('id', 'desc')->get();
-        $text1="";
+        $text1=$logs[0]->content;
         
-        foreach ($logs as $log) {
-            if (strpos($log->content, "Links")===false) {
-                $text1=$text1.$log->content."<br><br>";
-            }
-        }
-        
+
         $lastday_total = 0;
-        
+
         foreach ($users as $user) {
             $lastday = (($user->u+$user->d)-$user->last_day_t)/1024/1024;
             $lastday_total += (($user->u+$user->d)-$user->last_day_t);
-            
+
             if ($user->sendDailyMail==1) {
                 echo "Send daily mail to user: ".$user->id;
                 $subject = Config::get('appName')."-每日流量报告以及公告";
                 $to = $user->email;
                 $text = "下面是系统中目前的公告:<br><br>".$text1."<br><br>晚安！";
-                
+
                 try {
                     Mail::send($to, $subject, 'news/daily-traffic-report.tpl', [
                         "user" => $user,"text" => $text,"lastday"=>$lastday
@@ -48,9 +43,9 @@ class DailyMail
                 $text="";
             }
         }
-        
+
         $sts = new Analytics();
-        
+
         Telegram::Send("各位老爷少奶奶，我来为大家报告一下系统今天的运行状况哈~".
         PHP_EOL.
         "今日签到人数:".$sts->getTodayCheckinUser().PHP_EOL.
@@ -62,7 +57,7 @@ class DailyMail
 	public static function sendUserMessage() #sendUserMessage,给用户发送通知
     {
 		$users = User::all();
-		
+
         foreach ($users as $user) {
             if ($user->email_Notification==0) {
                 #管理员账户email_Notification设为1,用户设为0
@@ -81,11 +76,11 @@ class DailyMail
             }
         }
     }
-	
+
     public static function sendAdminMessage() #sendAdminMessage,给管理员发送通知,用于测试发送效果
     {
 		$users = User::all();
-		
+
         foreach ($users as $user) {
             if ($user->email_Notification==1) {
                 #管理员账户email_Notification设为1,用户设为0
@@ -104,7 +99,7 @@ class DailyMail
             }
         }
     }
-  
+
     public static function reall()
     {
         $users = User::all();
