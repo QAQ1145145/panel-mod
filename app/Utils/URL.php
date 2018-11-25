25813f3df1ce98a6f1700d92e01f551a7e9763fe
bfsdo0 <bfsdo0@gmail.com>
2018-11-25 14:07:02 +0800

    修改订阅部分逻辑，删除活动信息

--- app/Utils/URL.php
+++ app/Utils/URL.php
@@ -468,14 +468,14 @@ class URL
     }
 	
 	public static function getAnnouncement($user){
-		if($user->class !=0){
+		if($user->class ==100){
 			$ssurl = "127.0.0.1:1:auth_chain_a:chacha20:tls1.2_ticket_auth:YnJlYWt3YWxs/?obfsparam=&protoparam=&remarks=".Tools::base64_url_encode("双11活动,请关注官网")."&group=".Tools::base64_url_encode(Config::get('appName'));
 		}
       	return "ssr://".Tools::base64_url_encode($ssurl);
 	}
 	
 	public static function getUserTraffic($user){
-		if($user->class !=0){
+		if($user->class !=100){
 			$ssurl = "127.0.0.1:1:auth_chain_a:chacha20:tls1.2_ticket_auth:YnJlYWt3YWxs/?obfsparam=&protoparam=&remarks=".Tools::base64_url_encode("剩余流量：".number_format(($user->transfer_enable-($user->u+$user->d))/$user->transfer_enable*100,2)."% ".$user->unusedTraffic())."&group=".Tools::base64_url_encode(Config::get('appName'));
 		}else{
 			$ssurl = "127.0.0.1:1:auth_chain_a:chacha20:tls1.2_ticket_auth:YnJlYWt3YWxs/?obfsparam=&protoparam=&remarks=".Tools::base64_url_encode("账户已过期，请续费后使用")."&group=".Tools::base64_url_encode(Config::get('appName'));
